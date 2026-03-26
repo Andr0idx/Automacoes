07c91ee8236e00fa70d05c2e9167e82bc760a732
Gabriel Guedes <113951896+Andr0idx@users.noreply.github.com>
2026-03-26 02:07:08 -0300

    Update version number to 2.0

--- Automacao-WhatsApp-Biel.user.js
+++ Automacao-WhatsApp-Biel.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Automação WhatsApp Biel
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      2.0
 // @description  Script para automatizar Whatsapp
 // @author       Seu Nome
 // @match        https://web.whatsapp.com/*
